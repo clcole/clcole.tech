129f0350f1f0ac693a606ac524dabfef269ac91f
clcole <clcole@gmail.com>
2021-07-17 16:18:39 -0500

    icon animation

---
+++ js/icon-intersection.js
@@ -0,0 +1,30 @@
+let icons = [...document.querySelectorAll(".page-article__services > img")]; 
+
+function createIconObserver() {
+  let observer;
+
+  let options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: [0,.5,1]
+  };
+
+  observer = new IntersectionObserver(handleIconIntersect, options);
+  
+  icons.forEach((icon, index) => {
+    observer.observe(icon)
+  });
+}
+
+function handleIconIntersect(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio >= .75) {
+      entry.target.classList.add("icon-visible");
+    } 
+    else {
+      entry.target.classList.remove("icon-visible");
+    }
+  });
+}
+
+createIconObserver();
